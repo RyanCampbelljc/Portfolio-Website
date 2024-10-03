ce7c74e9c9433cadf13efed1c935c79dca1a34bc
Ryan Campbell <ryancampbelljc@gmail.com>
2024-10-03 10:50:34 -0300

    Update contacct submission form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,7 +8,36 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  nameError : HTMLDivElement | null = null;
+  emailError : HTMLDivElement | null = null;
+  messageError : HTMLDivElement | null = null;
+
+  ngAfterViewInit(){
+    this.nameError = document.getElementById("name-error-message") as HTMLDivElement;
+    this.emailError = document.getElementById("email-error-message") as HTMLDivElement;
+    this.messageError = document.getElementById("message-error-message") as HTMLDivElement;
+    //todo if any of these are null print an error
+  }
+
   onSubmit(name: string, email: string, message: string){
-    console.log(name);
+    let validForm: boolean = true;
+    if(name == ""){
+      this.nameError?.removeAttribute("hidden");
+      validForm = false;
+    }
+    if(email == ""){
+      this.emailError?.removeAttribute("hidden");
+      validForm = false;
+    }
+    if(message == ""){
+      this.messageError?.removeAttribute("hidden");
+      validForm = false;
+    }
+
+    if(validForm){
+      
+    }
+
+
   }
 }
