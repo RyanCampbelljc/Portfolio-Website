aa4849c09a84318675400ad25c0ec406f3df0701
Ryan Campbell <ryancampbelljc@gmail.com>
2024-10-12 18:50:12 -0300

    Made margins auto relative to a max width.
    Resizing window works so much better now

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,34 +8,39 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-  nameError : HTMLDivElement | null = null;
-  emailError : HTMLDivElement | null = null;
-  messageError : HTMLDivElement | null = null;
+  nameError! : HTMLDivElement;
+  emailError! : HTMLDivElement;
+  messageError! : HTMLDivElement;
+  form! : HTMLFormElement;
 
   ngAfterViewInit(){
     this.nameError = document.getElementById("name-error-message") as HTMLDivElement;
     this.emailError = document.getElementById("email-error-message") as HTMLDivElement;
     this.messageError = document.getElementById("message-error-message") as HTMLDivElement;
+    this.form = document.getElementById("content-input-container") as HTMLFormElement;
     //todo if any of these are null print an error
   }
 
   onSubmit(name: string, email: string, message: string){
     let validForm: boolean = true;
     if(name == ""){
-      this.nameError?.removeAttribute("hidden");
+      this.nameError.removeAttribute("hidden");
       validForm = false;
     }
     if(email == ""){
-      this.emailError?.removeAttribute("hidden");
+      this.emailError.removeAttribute("hidden");
       validForm = false;
     }
     if(message == ""){
-      this.messageError?.removeAttribute("hidden");
+      this.messageError.removeAttribute("hidden");
       validForm = false;
     }
 
     if(validForm){
-      
+      this.nameError.setAttribute("hidden","true");
+      this.emailError.setAttribute("hidden","true");
+      this.messageError.setAttribute("hidden","true");
+      this.form!.reset();
     }
 
 
--- src/app/services/project-card/projects.service.ts
+++ src/app/services/project-card/projects.service.ts
@@ -16,11 +16,12 @@ export class ProjectsService {
   // projects: Project[] =[];
 
   readonly CITY_DESCRIPTION: string = "This project is a 3D city simulation built using OpenGL. The city uses texture mapping and dynamic lighting to create a realistic environment. Users can explore the urban landscape with an interactive camera.";
-  readonly SCENE_MANAGEMENT_DESCRIPTION: string = "This project implements a scene management system in OpenGL using a quad tree structure and frustum culling for optimized rendering. Each object in the scene is represented by a node with a spherical bounding volume. Each top level node in a heiarchy is placed in the quadtree."
+  readonly SCENE_MANAGEMENT_DESCRIPTION: string = "This project implements a scene management system in OpenGL using a quadtree structure and frustum culling for optimized rendering. Each object in the scene is represented by a node with a spherical bounding volume. Each top level node in a heiarchy is placed in the quadtree."
   readonly PARTICLE_SYSTEM_DESCRIPTION: string = "Particle system designed to simulate real-time particle effects like fire, smoke, or explosions. The particle effects can be customized in XML files to change properties such as color, texture, duration, velocity, spawn rate, and pool size. Each particle emitter manages 1 pool of particles renders all particles in one draw call in order to improve efficiency."
   readonly PONG_DESCRIPTION: string = "A recreation of the classic pong game in TypeScript. Supports lobby creation in order to play with friends online.";
   readonly MINESWEEPER_DESCRIPTION: string = "A recreation of minesweeper. One of the first projects I did. Recently refactored in TypeScript."
-  
+  readonly WEBSITE_DESCRIPTION: string = "My first project using a frontend framework. Made in October 2024."
+
   readonly PROJECT_CITY: Project = {imgSrc: "assets/project-images/OpenglCity.png", name: "OpenGL City", description: this.CITY_DESCRIPTION, 
     skills: ["C++", "OpenGL"], links:[{logoClass: "fa-github", href: "https://github.com/RyanCampbelljc/OpenGL-City"}]};
   
@@ -42,7 +43,7 @@ export class ProjectsService {
       {logoClass: "fa-github", href: "https://github.com/RyanCampbelljc/Minesweeper"}]
     }
   
-  readonly PROJECT_WEBSITE: Project = {imgSrc: "", name: "This Website!", description: "",
+  readonly PROJECT_WEBSITE: Project = {imgSrc: "", name: "This Website!", description: this.WEBSITE_DESCRIPTION,
     skills: ["HTML", "CSS", "Angular", "Tailwind"], links:[{logoClass: "fa-github", href: "https://github.com/RyanCampbelljc/Portfolio-Website"}]}
       
 
