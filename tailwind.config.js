c322803705392f3510ee958ee6eb29ea98f0c7d1
Ryan Campbell <ryancampbelljc@gmail.com>
2024-10-11 21:32:54 -0300

    Project section tailwind save

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "paragraph-bg": "#2e2c2c",
+      },
+    },
   },
   plugins: [],
 };
