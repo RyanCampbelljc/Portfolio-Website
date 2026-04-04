76b8a410056beaa0dcae9c895e7648421b9b2f08
Ryan Campbell <ryancampbelljc@gmail.com>
2026-04-04 17:34:33 -0300

    Update description and text justification

--- src/app/services/about/about-images.service.ts
+++ src/app/services/about/about-images.service.ts
@@ -11,19 +11,18 @@ export class AboutImagesService {
 
   getSkillCards(): ImageInfo[]{
     return [
-      { title: 'HTML5', src: 'assets/HTML5.png' },
-      { title: 'CSS', src: 'assets/css.png' },
-      { title: 'Tailwind', src: 'assets/tailwind.png' },
       { title: 'TypeScript', src: 'assets/ts.png' },
-      { title: 'ExpressJS', src: 'assets/express.png' },
-      { title: 'Angular', src: 'assets/angular.png' },
-      { title: 'NodeJS', src: 'assets/node.png' },
-      { title: 'Python', src: 'assets/python.png' },
-      { title: 'C++', src: 'assets/c++.png' },
-      { title: 'OpenGL', src: 'assets/opengl.png' },
+      { title: 'Git', src: 'assets/git.png' },
       { title: 'Java', src: 'assets/java.png' },
+      { title: 'C++', src: 'assets/c++.png' },
+      { title: 'HTML5', src: 'assets/HTML5.png' },
+      { title: 'Tailwind', src: 'assets/tailwind.png' },
+      { title: 'CSS', src: 'assets/css.png' },
+      { title: 'Angular', src: 'assets/angular.png' },
+      { title: 'Python', src: 'assets/python.png' },
+      { title: 'OpenGL', src: 'assets/opengl.png' },
       { title: 'Godot', src: 'assets/godot.png' },
-      { title: 'Git', src: 'assets/git.png' }
+      { title: 'Cocos Creator', src: 'assets/cocos.png' },
     ]
   }  
 }
--- src/app/services/project-card/projects.service.ts
+++ src/app/services/project-card/projects.service.ts
@@ -20,18 +20,18 @@ export class ProjectsService {
   readonly PARTICLE_SYSTEM_DESCRIPTION: string = "Particle system designed to simulate real-time particle effects like fire, smoke, or explosions. The particle effects can be customized in XML files to change properties such as color, texture, duration, velocity, spawn rate, and pool size. Each particle emitter manages one pool of particles and renders all particles in one draw call in order to improve efficiency."
   readonly PONG_DESCRIPTION: string = "A recreation of the classic pong game in TypeScript. Supports lobby creation in order to play with friends online.";
   readonly MINESWEEPER_DESCRIPTION: string = "A recreation of minesweeper. One of the first projects I did. Recently refactored in TypeScript."
-  readonly WEBSITE_DESCRIPTION: string = "My first project using a frontend framework. Made in October 2024."
+  readonly WEBSITE_DESCRIPTION: string = "My first project using a frontend framework. First released in October 2024."
   readonly SUDOKU_DESCRIPTION: string = "A Sudoku solver / algorithm visualizer made with Python and Tkinter."
 
   readonly PROJECT_CITY: Project = {imgSrc: "assets/project-images/OpenglCity.png", name: "OpenGL City", description: this.CITY_DESCRIPTION, 
-    skills: ["C++", "OpenGL"], links:[{logoClass: "fa-github", href: "https://github.com/RyanCampbelljc/OpenGL-City"}]};
+    skills: ["C++", "OpenGL", "GLSL"], links:[{logoClass: "fa-github", href: "https://github.com/RyanCampbelljc/OpenGL-City"}]};
   
   readonly PROJECT_SCENE_MANAGMENT: Project = {imgSrc: "assets/project-images/SceneManagement.png", name: "Scene Management", description: this.SCENE_MANAGEMENT_DESCRIPTION,
     skills: ["C++", "OpenGL"], links:[{logoClass: "fa-github", href: "https://github.com/RyanCampbelljc/OpenGL-Scene-Management"}]
   }
   
   readonly PROJECT_PARTICLE_SYSTEM: Project = {imgSrc: "assets/project-images/particleSystem.png", name: "Particle System", description: this.PARTICLE_SYSTEM_DESCRIPTION,
-    skills: ["C++", "OpenGL"], links:[{logoClass: "fa-github", href: "https://github.com/RyanCampbelljc/OpenGL-Particle-System"}]
+    skills: ["C++", "OpenGL", "GLSL"], links:[{logoClass: "fa-github", href: "https://github.com/RyanCampbelljc/OpenGL-Particle-System"}]
   }
   
   readonly PROJECT_PONG: Project = {imgSrc: "assets/project-images/pong.png", name: "Pong", description: this.PONG_DESCRIPTION,
